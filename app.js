3aa73077d22ee68ad092a6f235b75331d7453110
ManuPro14 <103762387+ManuPro14@users.noreply.github.com>
2022-11-03 15:52:12 -0500

    se termina todo el diseño de la pagina incliyendo seccion de contacto y footer

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
 addEventListener('DOMContentLoaded', () => {
     const btn = document.querySelector('.btn_menu');
+    const section = document.querySelector('.section');
+
     if (btn){
         btn.addEventListener('click', () => {
             const menu_items = document.querySelector('.menu_items');
             menu_items.classList.toggle('show');
-        } )
+        })
     }
-})
+    if(section){
+        section.addEventListener('click', () =>{
+            const menu_items = document.querySelector('.menu_items');
+            menu_items.classList.toggle('ul.hide');
+        })
+    }
+   
+});
+
